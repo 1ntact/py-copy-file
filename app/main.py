a422c1b1aae68fd3650a6d651ae8fc9e0bdcd6dc
1ntact <verbaivan9@gmail.com>
2026-05-04 14:35:41 +0200

    Done!

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    if parts[0] != "cp":
+        return
+    source = parts[1]
+    destination = parts[2]
+
+    if source == destination:
+        return
+    try:
+        with open(source, "r") as f:
+            content = f.read()
+        with open(destination, "w") as f:
+            f.write(content)
+    except FileNotFoundError:
+        return
